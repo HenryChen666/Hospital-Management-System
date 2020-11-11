181a73d92829aba1ddf89e81b7b104c2d28e96bc
printbart <jklee4365@gmail.com>
2020-11-11 14:45:40 -0500

    base project

---
+++ backend/src/main/kotlin/seg3102/whmsapi/WhmsApiApplication.kt
@@ -0,0 +1,11 @@
+package seg3102.whmsapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhmsApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhmsApiApplication>(*args)
+}
---
+++ backend/src/test/kotlin/seg3102/whmsapi/WhmsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package seg3102.whmsapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WhmsApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
