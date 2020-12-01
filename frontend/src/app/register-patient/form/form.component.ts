dfd08f510f44c743d63b987c7d7548b8d251f293
Henrychen666 <henryc00183001@gmail.com>
2020-12-01 00:46:10 -0500

    Added firebase

---
+++ frontend/src/app/register-patient/form/form.component.ts
@@ -0,0 +1,100 @@
+import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormBuilder, Validators} from '@angular/forms';
+import {Patientdatas} from '../patientdatas';
+import {FormDbService} from './firestore/form-db.service';
+
+
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+
+
+
+export class FormComponent implements OnInit {
+  message: string;
+  hideMsg = true;
+  msgStyle = {
+    color: null,
+    'background-color': 'white',
+    'font-size': '150%',
+  };
+
+  users: Patientdatas[] = [];
+  userForm = this.fromBuilder.group({
+    phone: ['', [Validators.required, Validators.pattern('[1-9][0-9][0-9][1-9][0-9][0-9][0-9][0-9][0-9][0-9]')]],
+    firstName: ['', [Validators.required]],
+    lastName: ['', [Validators.required]],
+    dateofbirth: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]]
+  });
+  
+  get phone(): AbstractControl{return this.userForm.get('phone'); }
+  get firstName(): AbstractControl{return this.userForm.get('firstName'); }
+  get lastName(): AbstractControl{return this.userForm.get('lastName'); }
+  get email(): AbstractControl{return this.userForm.get('email'); }
+  get dateofbirth() : AbstractControl{return this.userForm.get('dateofbirth'); }
+
+  constructor(private  fromBuilder: FormBuilder, private store: FormDbService) { }
+
+  
+  ngOnInit(): void {
+    this.message = '';
+    this.store.getUsers().subscribe(data => {
+      this.users = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          ...(e.payload.doc.data() as object)
+        } as Patientdatas;
+      });
+    });
+  }
+
+  showMessage(type: string, msg: string): void {
+    this.msgStyle.color = type === 'error' ? 'red' : 'blue';
+    this.message = msg;
+    this.hideMsg = false;
+    setTimeout(
+      () => {
+        this.hideMsg = true;
+      }, 3500
+    );
+  }
+  
+  onSubmit(): void{
+
+    const user = new Patientdatas(
+      null,
+      this.userForm.value.lastName,
+      this.userForm.value.firstName,
+      Number(this.userForm.value.phone),
+      this.userForm.value.dateofbirth,
+      this.userForm.value.email);
+    this.users.push(user);
+    this.store.createUser(user)
+      .then(
+        docRef => {
+          user.id = docRef.id;
+          this.showMessage('info', 'Sucessfully Save');
+          }
+    )
+    .catch(_ =>
+      this.showMessage('error', 'Save Unsuccessful')
+    );
+    this.userForm.reset();
+
+  }
+
+  delete(id: string): void {
+    this.store.deleteUser(id)
+      .then(_ =>
+        this.showMessage('info', 'Sucessfully Delete')
+    )
+    .catch(_ =>
+      this.showMessage('error', 'Delete Unsuccessful')
+    );
+  }
+
+}
---
+++ frontend/src/app/register-patient/patientdatas.ts
@@ -0,0 +1,9 @@
+export class Patientdatas {
+    constructor(
+        public id: string,
+        public lastname: string,
+        public firstname: string,
+        public phone: number,
+        public dateofbirth: string,
+        public email: string) { }
+}
